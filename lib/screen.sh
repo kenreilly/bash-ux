ee8f84fa7dd19534260a76ad26c684f462f724b7
Kenneth Reilly <kenneth@innovationgroup.tech>
2019-10-01 01:22:17 -0700

    Added basic ANSI color functionality

--- lib/screen.sh
+++ lib/screen.sh
@@ -13,17 +13,29 @@ function draw_chars() {
 
 	local x=$1
 	local y=$2
-	local s=$3
+	local s="$3"
 
 	printf "\033[${y};${x}H${s}"
 }
 
+function print_debug() {
+
+	print_pid
+	print_dimensions
+}
+
 function print_dimensions() {
 	
-	local str="[ ${_screen['cols']} x ${_screen['rows']} ]"
-	local start=$((${_screen['cols']}-${#str}))
-
+	local str="[ $(set_color 'cyan')${_screen['cols']}$(set_color) x $(set_color 'cyan')${_screen['rows']}$(set_color) ]"
+	local start=$((${_screen['cols']}-$(str_length "$str")))
 	draw_chars $start 1 "$str"
+}
+
+function print_pid() {
+
+	local pid=$$
+	local str="[ PID: $(set_color 'red')${pid}$(set_color) ]"
+	draw_chars 1 1 "$str"
 }
 
 function screen_init() {
@@ -44,7 +56,7 @@ function screen_run() {
 
 	while [ : ]; do
 		[[ on_resize ]] && screen_init
-		[[ $DEBUG == 1 ]] && print_dimensions
+		[[ $DEBUG == 1 ]] && print_debug
 		sleep $delay
 	done
 }
